5ccf554a30d5bc43e989a6674364bc0aea484f92
Shubham-Grg555 <shubhamgrg555@gmail.com>
2026-01-30 16:11:47 +0000

    Removed some unnecessary code.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,15 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import vitest from 'eslint-lugin-vitest'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}','**/*.test.js', '**/*.spec.js', '**/*.test.jsx', '**/*.spec.jsx'],
-    plugins: {
-        vitest
-    },
+    files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -19,10 +15,7 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        browser,
-        ...vitest.environments.env.globals,
-      },
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -31,7 +24,6 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      ...vitest.configs.recommended.rules,
     },
   },
 ])
