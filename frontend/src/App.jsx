ef281f55693573b69cf7d579f6f95fc7f5eccd1a
Milap Keshwala <mkeshwala06@gmail.com>
2026-01-30 19:17:04 +0000

    Adds wrapped and playlist cover pages to url route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MainTheme from "./styles/Theme";
+import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
+import Wrapped from "./pages/Wrapped";
+import CustomCovers from "./pages/CustomCovers";
 import NotFound from "./pages/NotFound";
 
 function App() {
     return (
-        <BrowserRouter>
+        <ThemeProvider theme={MainTheme}>
+            <CssBaseline></CssBaseline>
+            <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Home />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/about-us" element={<AboutUs />} />
                     <Route path="/contact" element={<Contact />} />
+                    <Route path="/your-wrapped" element={<Wrapped />} />
+                    <Route
+                        path="/custom-playlist-covers"
+                        element={<CustomCovers />}
+                    />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-        </BrowserRouter>
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
