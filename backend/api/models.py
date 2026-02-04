a8e50c22746f3b177257b619175ca2e9f0a31dd0
Shubham-Grg555 <shubhamgrg555@gmail.com>
2026-02-04 15:53:27 +0000

    Improved and renamed the model storing the Spotify tokens.

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
 class SpotifyAccountModel(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, max_length=50)
     access_token = models.CharField(max_length=255)
     refresh_token = models.CharField(max_length=255)
-    access_token_expires_at = models.DateTimeField()
+    token_expires_in = models.DateTimeField()
+    token_type = models.CharField(max_length=50)
